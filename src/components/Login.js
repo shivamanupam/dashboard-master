b711e0907c8e9c2d9f9d615875e081dfc3642ce5
sachin arya <sachinarya0801@gmail.com>
2024-10-04 17:47:47 +0530

    form input field added

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,12 @@ function Login() {
   return (
     <div>
       <h2>login here</h2>
+      <div>
+        <input type="text" placeholder="username" />
+      </div>
+      <div>
+        <input type="password" placeholder="password" />
+      </div>
     </div>
   );
 }
