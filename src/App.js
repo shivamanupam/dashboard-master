57de4f1f60a006e617e1b4681f6742ba0259feb5
Shivam Anupam <shivamanupam@Shivams-MacBook-Air.local>
2024-10-04 16:10:46 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello World!</h2>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <h2>Dashboard</h2>
+      <span>Hello World</span>
+      <ul>
+        <li className="lint">About</li>
+        <li className="lint">Contact</li>
+        <li className="lint">Login</li>
+      </ul>
+    </>
+  );
+};
+
+export default Dashboard;
