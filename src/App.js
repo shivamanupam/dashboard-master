02fc6ebf43253d60a63bae3329b9b17eb7d25858
Shivam Anupam <shivamanupam@Shivams-MacBook-Air.local>
2024-10-04 17:40:25 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
 import Login from "./components/Login";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
       <Dashboard />
       <Login />
+      <Footer />
     </div>
   );
 }
